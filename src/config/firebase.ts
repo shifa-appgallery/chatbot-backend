2fa0c495231827c78a37eeecd53a96e0612dc025
Shifa <shifa.appgallery@gmail.com>
2026-04-03 16:50:46 +0530

    changes

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,6 +4,7 @@ const PROJECT_ID = "wefroth-3b0c9";
 
 export const getAccessToken = async (): Promise<string> => {
   const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!);
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
   const auth = new google.auth.GoogleAuth({
     credentials: serviceAccount,
